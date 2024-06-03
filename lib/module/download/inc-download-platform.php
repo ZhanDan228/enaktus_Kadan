effae9afd12f7e9e2d1053cecd0b74b1d2bb03fc
KW2290 <kanatturusbaev245@gmail.com>
2024-06-03 22:32:22 +0600

    asas

---
+++ lib/module/download/inc-download-platform.php
@@ -0,0 +1,188 @@
+<div class='downloadable-con-container'>
+	<div class='downloadable-con-outer'>
+		<div class='downloadable-con-inner-a'>
+			<div class='downloadable-con-graphic' style="background: center top no-repeat; right: -52px; bottom: -38px;">
+			</div>
+			<div class='downloadable-con-image' style="background: url(/img/antsCafeLogo.png) center left / 42px no-repeat">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>ANT'S</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>08:30 - 22:30
+					Без выходных
+					<br>бул. Эркиндик, д. 35
+				</span>
+			</div>
+			<a href="https://www.instagram.com/ants.bishkek/">
+			<div class='package-con-button'>
+				<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+				</div>
+				<div class='package-tx1-button'>
+					<span>Instagram</span>
+				</div>
+			</div>
+			</a>
+			<style>
+.package-con-button {
+    background: #833ab4;
+    background: linear-gradient(
+        to right,
+        #833ab4, #fd1d1d, #fcb045
+    );
+    border: 2px solid red; /* Здесь указано свойство border для создания границы */
+}
+
+.package-con-button:hover {
+    transform: scale(1.1); /* Увеличиваем размер кнопки на 10% при наведении курсора */
+	background: #833ab4;
+    background: linear-gradient(
+        to right,
+        #833ab4, #fd1d1d, #fcb045
+    );
+    border: 2px solid red; /* Здесь указано свойство border для создания границы */
+}
+
+			</style>
+			<?php
+			if (isset($build) && !is_null($build->get_url_windows()))
+				printf("</a>");
+			?>
+		</div>
+	</div>
+	<div class='downloadable-con-outer'>
+		<div class='downloadable-con-inner-a'>
+			<div class='downloadable-con-graphic' style="backgrou center top no-repeat; right: -52px; bottom: -38px;">
+			</div>
+			<div class='downloadable-con-image' style="background: url(/img/didarCafeLogo.png) center left / 42px no-repeat;">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>Дидар</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>Ежедневно с 10:00 до 24:00
+					<br>Анарбека Бакаева, 42
+				</span>
+			</div>
+			<a href="https://www.instagram.com/didar_chaihana/">
+			<div class='package-con-button'>
+				<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+				</div>
+				<div class='package-tx1-button'>
+					<span>Instagram</span>
+				</div>
+			</div>
+			</a>
+			<?php
+			if (isset($build) && !is_null($build->get_url_linux()))
+				printf("</a>");
+			?>
+		</div>
+	</div>
+	<div class='downloadable-con-outer'>
+		<div class='downloadable-con-inner-a'>
+			<div class="downloadable-con-graphic" style="backgrou center top no-repeat;right: -74px;bottom: -128px;width: 246px;height: 272px;">
+			</div>
+			<div class='downloadable-con-image darkmode-invert' style="background: url(/img/defaultCafeLogo.png) center left / 42px no-repeat;">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>Таберик</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>Ежедневно с 10:00 до 22:00
+				<br>Асаналиева, 99
+				</span>
+			</div>
+			<a href="https://www.instagram.com/restoran_taberik/">
+			<div class='package-con-button'>
+				<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+				</div>
+				<div class='package-tx1-button'>
+					<span>Instagram</span>
+				</div>
+			</div>
+			</a>
+			<?php
+			if (isset($build) && !is_null($build->get_url_mac()))
+				printf("</a>");
+			?>
+		</div>
+	</div>
+</div>
+<div class='downloadable-con-container'>
+	<div class='downloadable-con-outer'>
+		<div class='downloadable-con-inner-a'>
+			<div class='downloadable-con-graphic' style="backgroun center top no-repeat; right: -52px; bottom: -38px;">
+			</div>
+			<div class='downloadable-con-image' style="background: url(/img/defaultCafeLogo.png) center left / 42px no-repeat;">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>Family</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>Ежедневно с 10:00 до 23:00
+					<br>Садырбаева, 251</span>
+			</div>
+			<a href="https://www.instagram.com/family_cafe_kg/">
+			<div class='package-con-button'>
+				<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+				</div>
+				<div class='package-tx1-button'>
+					<span>Instagram</span>
+				</div>
+			</div>
+			</a>
+		</div>
+	</div>
+	<div class='downloadable-con-outer'>
+		<div class='downloadable-con-inner-a'>
+			<div class='downloadable-con-graphic' style="backgroun center top no-repeat; right: -52px; bottom: -38px;">
+			</div>
+			<div class='downloadable-con-image' style="background: url(/img/lesnoy.png) center left / 42px no-repeat;">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>Cafe-bar "Lesnoy"</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>Ежедневно с 07:30 до 01:00
+					<br>​Улица Ибраимова, 70</span>
+			</div>
+			<a href="https://www.instagram.com/lesnoy.kg/">
+			<div class='package-con-button'>
+				<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+				</div>
+				<div class='package-tx1-button'>
+					<span>Instagram</span>
+				</div>
+			</div>
+			</a>
+		</div>
+	</div>
+	<!-- <div class='downloadable-con-outer' style="width: 66.6666666666%;">
+		<div class='downloadable-con-inner-a'>
+			<div class='downloadable-con-graphic' style="background center top no-repeat; right: -52px; bottom: -38px;">
+			</div>
+			<div class='downloadable-con-image darkmode-invert' style="background: url(/img/defaultCafeLogo.png) center left / 42px no-repeat;">
+			</div>
+			<div class='downloadable-tx1-title darkmode-txt'>
+				<span>Previous Builds</span>
+			</div>
+			<div class='downloadable-tx2-desc darkmode-txt'>
+				<span>Checking previous builds allows you browse all publicly released official builds. We offer useful metadata for each build such as SHA, OS version, author and the commit it was compiled from. Please be aware that because these are previous builds they may be missing important fixes and additions found in later builds.<br><br>For furthers details see our <a href="https://github.com/RPCS3/rpcs3/commits/master" target="_blank">commits</a> log via GitHub.</span>
+				</span>
+				<br><br>
+			</div>
+			<a href="https://rpcs3.net/compatibility?b">
+				<div class='package-con-button' style="width: 225px;">
+					<div class='package-ico-button' style="background: url(/img/instminilogo.png) center / 22px no-repeat;">
+					</div>
+					<div class='package-tx1-button'>
+						<span>
+							Instagram
+						</span>
+					</div>
+				</div>
+			</a>
+		</div> -->
+	</div>
+</div>
